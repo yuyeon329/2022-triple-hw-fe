503bcb8c4726df494aa84f3638b5f340ea837cbb
yuyeon329 <peachberryjr@gmail.com>
2022-07-05 18:06:41 +0900

    start prj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import React from 'react';
+// 404
+import NotFound from "./pages/NotFound";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* Header 중첩 라우팅 */}
+      {/* <Route> */}
+        {/* Footer 중첩 라우팅*/}
+        {/* <Route></Route> */}
+      {/* </Route> */}
+      {/* 404 */}
+      <Route path="*" element={<NotFound/>}></Route>
+    </Routes>
   );
 }
 

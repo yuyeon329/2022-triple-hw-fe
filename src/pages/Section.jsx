c1575ef7da059235b451c81378d89d42578e1dbd
yuyeon329 <peachberryjr@gmail.com>
2022-07-07 11:39:12 +0900

    update triple component

--- src/pages/Section.jsx
+++ src/pages/Section.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import styled from 'styled-components';
+import Triple from './../pages/Section/Triple';
 
 const StyledSection = styled.div`
   background-color : red;
-  height : 20rem;
+  height : 35rem;
+`;
+
+const StyledDiv = styled.div`
+  background-color : blue;
+  width : 80rem;
+  height : 35rem;
+  margin : auto;
 `;
 
 const Section = () => {
   return(
     <div>
-      <StyledSection>This is HomeWork Section!!
-      </StyledSection>
+      <StyledSection>
+        <StyledDiv>
+          <Triple></Triple>
+        </StyledDiv>
+        </StyledSection>
     </div>
   );
 }
--- src/pages/Section/Triple.jsx
+++ src/pages/Section/Triple.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
+import appImage from './../../assets/images/triple2x.png';
+
+const StyledImage = styled.img.attrs({
+  src : `${appImage}`
+})`
+  margin-top : 178px;
+  width : 400px;
+  height : auto;
+`;
+
 const Triple = () => {
   return(
     <>
+    <StyledImage></StyledImage>
     </>
   );
 };
